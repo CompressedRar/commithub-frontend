05794536670d8ad2348dbce2219e7bf13d5d6e7a
CompressedRar <qwertythanzip1103@gmail.com>
2025-10-21 11:01:09 +0800

    changed

--- src/components/api.jsx
+++ src/components/api.jsx
@@ -35,10 +35,10 @@ export function checkRole(){
 const backend_url = import.meta.env.VITE_BACKEND_URL;
 
 console.log("BACKEND: ", backend_url)
-export var socket = io(backend_url)
+export var socket = io("http://15.134.32.170:5000")
 
 const api = axios.create({
-        baseURL: backend_url,
+        baseURL: "http://15.134.32.170:5000",
         headers: {
           Authorization: `Bearer ${token}`
         }
